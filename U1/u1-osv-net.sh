5413ee0b8dde70283422c69ad9105f6f5b12bcdf
Ilan K <ilan.keller@gmail.com>
2023-02-03 23:11:18 +0000

    Working OSV tests

--- U1/u1-osv-net.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export OSV_BRIDGE=lan-rsx217
-brctl addif $OSV_BRIDGE $1
-brctl stp $OSV_BRIDGE off
-ip address add 172.102.0.5/32 dev $1
-ifconfig $1 up
--- osv-runner.sh
+++ osv-runner.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#!/bin/sh
-
-set_vInterfaces(){
+set_vDevices(){
     #lan-rsx217
     sudo brctl addbr br-lan-rsx217
-    sudo ip link add name eth-lan-rsx217 type dummy
-    sudo ip addr add 172.102.0.1/24 broadcast 172.102.0.255 dev eth-lan-rsx217 
-    sudo brctl addif br-lan-rsx217 eth-lan-rsx217
+    sudo ip addr add 172.102.0.1/24 broadcast 172.102.0.255 dev br-lan-rsx217 
     # F1 lan
     sudo ip tuntap add dev tap_F1_lan mode tap
     sudo brctl addif br-lan-rsx217 tap_F1_lan
@@ -16,48 +12,42 @@ set_vInterfaces(){
     # U2 lan
     sudo ip tuntap add dev tap_U2_lan mode tap
     sudo brctl addif br-lan-rsx217 tap_U2_lan
+
     #wan-rsx217
     sudo brctl addbr br-wan-rsx217
-    sudo ip link add name eth-wan-rsx217 type dummy
-    sudo brctl addif br-wan-rsx217 eth-wan-rsx217
-    sudo ip addr add 172.101.0.1/24 broadcast 172.101.0.255 dev eth-wan-rsx217
-    
-    #brctl addif br-wan-rsx217 eth-wan-rsx217
-    #F1 wan
+    sudo ip addr add 172.101.0.1/24 broadcast 172.101.0.255 dev br-wan-rsx217
+    #F1 on wan
     sudo ip tuntap add dev tap_F1_wan mode tap
     sudo brctl addif br-wan-rsx217 tap_F1_wan
-    #U3 wan
+    #U3 on wan
     sudo ip tuntap add dev tap_U3_wan mode tap
     sudo brctl addif br-wan-rsx217 tap_U3_wan
 
-    # UP!
-    sudo ifconfig eth-lan-rsx217 up
-    # sudo ifconfig br-lan-rsx217 up
-    sudo ifconfig eth-wan-rsx217 up
-    # sudo ifconfig br-wan-rsx217 up
+    # dnsmasq for dhcp static ip on mac adress
+    sudo dnsmasq --conf-file=$PWD/dnsmasq-utils/dnsmasq-lan-rsx217.conf
+    sudo dnsmasq --conf-file=$PWD/dnsmasq-utils/dnsmasq-wan-rsx217.conf
+
+    # Up devices
+    sudo ifconfig br-lan-rsx217 up
+    sudo ifconfig br-wan-rsx217 up
     sudo ifconfig tap_F1_lan up
     sudo ifconfig tap_U1_lan up
     sudo ifconfig tap_U2_lan up
     sudo ifconfig tap_F1_wan up
     sudo ifconfig tap_U3_wan up
-
-    # dnsmasq for dhcp static ip on mac adress
-    sudo dnsmasq --conf-file=$PWD/dnsmasq-utils/dnsmasq-lan-rsx217.conf
-    sudo dnsmasq --conf-file=$PWD/dnsmasq-utils/dnsmasq-wan-rsx217.conf
+    echo "END set Interfaces"
 }
 
-down_vInterfaces(){
+down_vDevices(){
     sudo ifconfig br-lan-rsx217 down
-    sudo ifconfig eth-lan-rsx217 down
     sudo ifconfig br-wan-rsx217 down
-    sudo ifconfig eth-wan-rsx217 down
     sudo ifconfig tap_F1_lan down
     sudo ifconfig tap_U1_lan down
     sudo ifconfig tap_U2_lan down
     sudo ifconfig tap_F1_wan down
     sudo ifconfig tap_U3_wan down
-    sudo ip addr delete 172.102.0.1/24 dev eth-lan-rsx217
-    sudo ip addr delete 172.101.0.1/24 dev eth-wan-rsx217
+    sudo ip addr delete 172.102.0.1/24 dev br-lan-rsx217
+    sudo ip addr delete 172.101.0.1/24 dev br-wan-rsx217
     sudo ip tuntap del dev tap_F1_lan mode tap
     sudo ip tuntap del dev tap_U1_lan mode tap
     sudo ip tuntap del dev tap_U2_lan mode tap
@@ -66,9 +56,8 @@ down_vInterfaces(){
     sudo brctl delbr br-lan-rsx217
     sudo brctl delbr br-wan-rsx217
     sudo killall dnsmasq
+    sudo killall qemu-system-x86_64
 }
-#QEMU U1
-#run_qemu $1 osv_file_path $2 tap_device $3 mac_addr
 run_qemu(){
     sudo qemu-system-x86_64 \
          -chardev stdio,mux=on,id=stdio -mon chardev=stdio,mode=readline \
@@ -78,10 +67,6 @@ run_qemu(){
          -device virtio-rng-pci \
          -nographic
 }
-         #-virtfs local,path=$PWD/logs,mount_tag=logs,security_model=passthrough,id=logs \
-#         -netdev tap,id=hn0,script=$2,vhost=on \
-#         -device virtio-net-pci,netdev=hn0,id=nic1 \
-#run_f1 $1 osv_file_path $2 tap_device $3 mac_addr $4 tap_device2 
 run_f1(){
     sudo qemu-system-x86_64 \
          -chardev stdio,mux=on,id=stdio -mon chardev=stdio,mode=readline \
@@ -94,25 +79,15 @@ run_f1(){
          -nographic
 
 }
-         #-virtfs local,path=$PWD/logs,mount_tag=logs,security_model=passthrough,id=logs \
-run_osv(){
-    ~/osv/scripts/run.py -i $osv_image
-}
-
-set_vInterfaces
-# #sudo ../osv/scripts/run.py --verbose -n -b lan-rsx217 --ip=172.102.0.5 -i osv-images/u1-osv-rsx217/u1-osv-rsx217.qemu
-# # #U1
-# $(run_qemu osv-images/u1-osv-rsx217/u1-osv-rsx217.qemu tap_U1_lan 2c:4d:11:12:11:01) &
-# # echo U1 UP!
-# # #U2
-# $(run_qemu osv-images/u2-osv-rsx217/u2-osv-rsx217.qemu tap_U2_lan 2c:4d:11:12:11:02) &
-# # echo U2 UP!
-# # #F1
-# $(run_f1 osv-images/f1-osv-rsx217/f1-osv-rsx217.qemu tap_F1_lan 2c:4d:11:12:11:10 tap_F1_wan) &
-# # echo F1 UP!
-# #U3
-# sleep 5
-# run_qemu osv-images/u3-osv-rsx217/u3-osv-rsx217.qemu tap_U3_wan 2c:4d:11:12:11:03
-down_vInterfaces
+set_vDevices
+#U1
+$(run_qemu osv-images/u1-osv-rsx217/u1-osv-rsx217.qemu tap_U1_lan 2c:4d:11:12:11:01) &
+#U2
+$(run_qemu osv-images/u2-osv-rsx217/u2-osv-rsx217.qemu tap_U2_lan 2c:4d:11:12:11:02) &
+#F1
+$(run_f1 osv-images/f1-osv-rsx217/f1-osv-rsx217.qemu tap_F1_lan 2c:4d:11:12:11:10 tap_F1_wan) &
+#U3
+run_qemu osv-images/u3-osv-rsx217/u3-osv-rsx217.qemu tap_U3_wan 2c:4d:11:12:11:03
+down_vDevices
 
 
